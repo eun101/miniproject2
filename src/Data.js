c432c76314369045174264fd97a1554010c8448f
ennaesor <sherylvelasco340@gmail.com>
2022-11-09 23:43:34 -0300

    searchbar

---
+++ src/Data.js
@@ -0,0 +1,73 @@
+
+
+ let data = [
+{
+  "title": "course",
+  "language": "english",
+},
+{
+  "title": "community",
+  "language": "english",
+},
+{
+  "title": "pricing",
+  "language": "english",
+},
+{
+  "title": "success stories",
+  "language": "english",
+},
+{
+  "title": "about us",
+  "language": "english",
+},
+{
+  "title": "contact us",
+  "language": "english",
+},
+{
+  "title": "log in",
+  "language": "english",
+},
+{
+  "title": "footer",
+  "language": "english",
+},
+{
+  "title": "all courses",
+  "language": "english",
+},
+{
+  "title": "social media",
+  "language": "english",
+},
+{
+  "title": "success stories of members",
+  "language": "english",
+},
+{
+  "title": "personal development",
+  "language": "english",
+},
+{
+  "title": "financial education",
+  "language": "english",
+},
+{
+  "title": "speaking",
+  "language": "english",
+},
+{
+  "title": "facebook",
+  "language": "english",
+},
+{
+  "title": "instagram",
+  "language": "english",
+},
+{
+  "title": "twitter",
+  "language": "english",
+},
+
+]
